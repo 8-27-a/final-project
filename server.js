bbe9685d03b605182368f5c5d6bbad6e7c7080e9
Leen Kim <llcoolk@MACBKPROi7.fios-router.home>
2018-09-07 12:16:32 -0700

    added console.log server port number

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 // module imports
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const app = express();
 
 // PRODUCTION ONLY
-app.use(express.static(path.join(__dirname, 'client/build')));
-
+if (process.env.NODE_ENV) {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 // app middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // PRODUCTION ONLY
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+if (process.env.NODE_ENV) {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
 
 // Development mode port
 const port = process.env.PORT || 5000;
-app.listen(port)
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 module.exports = app;
