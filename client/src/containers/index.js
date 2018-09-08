74025c8d152b1057127af4c6a7ff022d12926b4f
Cody Jacobs <codered686@gmail.com>
2018-09-08 17:11:47 -0400

    setup frontend project structure

---
+++ client/src/containers/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from './Home';
+export { default as Register } from './Register';
+export { default as Profile } from './Profile';
+export { default as Appointment } from './Appt';
+export { default as Login } from './Login';
+
