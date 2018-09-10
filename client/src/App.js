adafeadb9a809ff2df973f05db41c23251ec3ddb
Dane Shambrook <daxisda1@gmail.com>
2018-09-10 10:27:44 -0700

    added nav bar & footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-//import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Home, Appointment, Profile, Login, Register } from "./containers";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
-        <h1 className="App-intro">
-          This is where our Mentor4Me app will reside!
-        </h1>
-      </div>
+      <Router>
+        <React.Fragment>
+          <Header brand="Mentors 4 Me" />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Profile" component={Profile} />
+          <Route exact path="/Appt" component={Appointment} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Footer />
+        </React.Fragment>
+      </Router>
     );
   }
 }
 
 export default App;
+ 
