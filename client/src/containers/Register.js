3e3d1da23bdab8a207df2e161507b01690920e9d
Cody Jacobs <codered686@gmail.com>
2018-09-17 23:07:09 -0400

    change radio buttons to selection method for user role

--- client/src/containers/Register.js
+++ client/src/containers/Register.js
@@ -5,12 +5,12 @@ import { API_URL } from "../backend_api";
 
 class Register extends Component {
   state = {
-    email: "",
-    password: "",
-    password2: "",
     first: "",
     last: "",
     role: "",
+    email: "",
+    password: "",
+    password2: "",
     errors: {}
   };
 
@@ -19,20 +19,26 @@ class Register extends Component {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  // handleRoleChange = e => {
+  //   this.setState.role === ""
+  //     ? [e.target.role] === "mentor"
+  //     : [e.target.role] === "student";
+  // };
+
   handleSubmit = e => {
     e.preventDefault();
 
-    const { email, password, password2, first, last, role } = this.state;
+    const { first, last, role, email, password, password2 } = this.state;
     const errors = this.validate({ email, password, password2 });
     this.setState({ errors });
 
     if (Object.keys(errors).length === 0) {
       Axios.post(`${API_URL}/signup`, {
-        email,
-        password,
         first,
         last,
-        role
+        role,
+        email,
+        password
       }).then(user => {
         if (user.status === 201) {
           this.props.history.push("/login");
@@ -74,6 +80,9 @@ class Register extends Component {
                 onChange={this.handleChange}
                 onSubmit={this.handleSubmit}
                 errors={this.state.errors}
+                first={this.state.first}
+                last={this.state.last}
+                role={this.state.role}
                 email={this.state.email}
                 password={this.state.password}
                 password2={this.state.password2}
