6ea23fce1aae4ca9d7a0efab9b4fe03b6db3175a
Leen Kim <leenkim@alumni.usc.edu>
2018-09-25 15:35:05 -0700

    API version fix in frontend configuration

--- client/src/backend_api.js
+++ client/src/backend_api.js
@@ -2,9 +2,9 @@ let backendUrl;
 const version = "v1";
 
 if (window.location.hostname === "localhost") {
-  backendUrl = "http://localhost:5000/v1";
+  backendUrl = `http://localhost:5000/${version}`;
 } else {
-  backendUrl = `${window.location.origin}/version`;
+  backendUrl = `${window.location.origin}/${version}`;
 }
 
 export const API_URL = backendUrl;
