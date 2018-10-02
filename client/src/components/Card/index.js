a044d4b82e079aef0cd955dda5637097200bb773
Leen Kim <leenkim@alumni.usc.edu>
2018-10-02 16:41:00 -0700

    style change + other fixes

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,61 +1,71 @@
 import React from "react";
 
 const Card = ({ first, last, email, role, imgUrl, summary, bio, userId }) => (
-  <div
-    className="card mt-5"
-    style={{
-      backgroundColor: "rgba(230, 230, 230, .6)"
-    }}
-  >
-    <h1
-      className="card-title"
-      style={{
-        color: "hsl(0,0%,13%)",
-        fontWeight: 700,
-        paddingLeft: 22,
-        paddingTop: 10
-      }}
-    >{`${first} ${last}`}</h1>
-    <img
-      className="mx-auto rounded-circle"
-      src={imgUrl ? imgUrl : "/images/no-avatar.png"}
-      alt={`${first} ${last}`}
-      height="300"
-      width="300"
-    />
+  <div className="container">
+    {/* <div>
+      <h2 className="profile text-dark text-center">{`${role}`}</h2>
+    </div> */}
     <div
-      className="email text-center"
-      style={{ paddingTop: 10, paddingBottom: 5 }}
-    >
-      <a href={`mailto:${email}`} target="_blank" rel="noopener noreferrer">
-        <i className="fa fa-envelope-o"> {`${email}`}</i>
-      </a>
-    </div>
-    <div
-      className="card-body"
+      className="card text-dard"
       style={{
-        backgroundColor: "rgba(255, 255, 255, 0.7)"
+        marginTop: 10
       }}
     >
-      <h3
-        className="card-text"
+      <h1
+        className="card-title text-dark"
         style={{
-          color: "hsl(0, 0%, 15%)",
-          fontWeight: 500,
-          paddingBottom: 15
+          // color: "rgba(255,255,255, 1)",
+          fontWeight: 700,
+          paddingLeft: 22,
+          paddingTop: 10
+        }}
+      >{`${first} ${last}`}</h1>
+      <img
+        className="mx-auto rounded-circle"
+        src={imgUrl ? imgUrl : "/images/no-avatar.png"}
+        alt={`${first} ${last}`}
+        height="300"
+        width="300"
+      />
+      <div
+        className="email text-center"
+        style={{
+          paddingTop: 10,
+          paddingBottom: 5,
+          color: "rgba(49, 205, 232, 1)"
+          // backgroundColor: "rgba(255, 255, 255, 0.7)"
         }}
       >
-        {summary}
-      </h3>
-      <h5
-        className="card-text"
+        <a href={`mailto:${email}`} target="_blank" rel="noopener noreferrer">
+          <i className="fa fa-envelope-o"> {`${email}`}</i>
+        </a>
+      </div>
+      <div
+        className="card-body"
         style={{
-          color: "hsl(0, 0%, 45%)",
-          paddingBottom: 75
+          backgroundColor: "rgba(49, 205, 232, .4)"
         }}
       >
-        {bio}
-      </h5>
+        <h3
+          className="card-text"
+          style={{
+            color: "hsl(0, 0%, 15%)",
+            fontWeight: 500,
+            paddingBottom: 15
+          }}
+        >
+          {summary}
+        </h3>
+        <h5
+          className="card-text"
+          style={{
+            color: "hsl(0, 0%, 45%)",
+            paddingBottom: 70
+          }}
+        >
+          {bio}
+        </h5>
+      </div>
     </div>
   </div>
 );
